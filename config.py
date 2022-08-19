714d382021e2b36a1b36e2bd93bad1b216bf5059
Amir H. Zakaria <76696009+AHZak@users.noreply.github.com>
2022-08-19 20:51:33 +0400

    website url added in about

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ APOD_URL        = "https://api.nasa.gov/planetary/apod"
 ### BOT
 MSG = {
     "START"     : "Welcome to APOD bot!\nIt gives you access to NASA's Astronomy Picture of the Day archive in your comfort on Telegram.\nYou can\n• pass a date(like 2020-2-22)\n• use /today for the most recent picture\n• or let the bot surprise you by /random !\n\nThis is an open-source project.\nSee About for more information.\n\nWith 💛 by Amir",
-    "ABOUT"     : "I was wondering if it would be more convenient to access NASA's APOD archive from a Telegram bot, so I made it happen :)\nThis is an open-source project. You can find the source code on <a href='https://github.com/AHZak/APOD_NASAbot'>Github</a>\nTo report bugs and for suggestions, please contact me via\npro@zakariazadeh.com\n\n∆ by Amir Zakaria",
+    "ABOUT"     : "I was wondering if it would be more convenient to access NASA's APOD archive from a Telegram bot, so I made it happen :)\nThis is an open-source project. You can find the source code on <a href='https://github.com/AHZak/APOD_NASAbot'>Github</a>\nTo report bugs and for suggestions, please contact me via\npro@zakariazadeh.com\n\n∆ by <a href='https://zakariazadeh.com'>Amir Zakaria</a>",
     'ARCHIVE'   : "An archinve holding more than 27 years of daily APOD pictures!\nSimply send me a date from Jun 16, 1995 in the following format:\n2022-2-22",
     "UNKNOWN"   : "I don't know what's happening 🤔\nI'll inform Amir",
     'OPTIONS'   : "You can send me /today" + ", " + "/random or a date in the following format:\n2020-2-22",
